784e8b2ea0773b3ddbf3ce114211fad64c4a73c8
GauravPachbhai <gauravpachbhai555@gmail.com>
2022-11-14 19:26:08 +0530

    code

---
+++ home.js
@@ -0,0 +1,35 @@
+const cityUrl = "https://zomatoajulypi.herokuapp.com/location"
+const restUrl = "https://zomatoajulypi.herokuapp.com/restaurant?stateId="
+
+function getcity(){
+    fetch(cityUrl,{method:'GET'})
+    .then((rest) => rest.json())
+    .then((data) => {
+        data.map((item) => {
+            let element = document.createElement('option')
+            let text = document.createTextNode(item.state)
+            element.appendChild(text)
+            element.value = item.state_id
+            document.getElementById('city').appendChild(element)
+
+        })
+    })
+}
+
+function getreset(){
+    let cityId = document.getElementById('city').value;
+    let rest  = document.getElementById('reselect');
+    // while (rest.length >0){
+    //     rest.remove(0)
+    // }
+    fetch(`${restUrl},${cityId}`,{method:'GEt'})
+    .then((rest) => rest.json())
+    .then((data) => {
+        data.map((item) => {
+            let element = document.createElement('option')
+            let text = document.createTextNode(item.restaurant_name)
+            element.appendChild(text)
+            rest.appendChild(element)
+        })
+    })
+}
